89c2216712cba804dc102a199ed59968f3538bcc
s421431 <svjadhav1482@gmail.com>
2025-07-13 18:37:45 +0530

    Add files via upload

---
+++ highlights.js
@@ -0,0 +1,311 @@
+import { Description } from "@mui/icons-material";
+
+const highlights = [
+    {
+      title: 'Ideathon Competition',
+      images: [
+        '/ideathon/image (1)',
+        '/ideathon/image (2)',
+        '/ideathon/image (3)',
+        '/ideathon/image (5)',
+        '/ideathon/image (6)',
+        '/ideathon/image (9)',
+        '/ideathon/image (10)',
+        '/ideathon/image (11)',
+        '/ideathon/image (12)',
+        '/ideathon/image (13)',
+        '/ideathon/image (14)',
+        '/ideathon/image (15)',
+        '/ideathon/image (16)',
+        '/ideathon/image (17)',
+        '/ideathon/image (18)',
+        '/ideathon/image (19)',
+        '/ideathon/image (20)',
+        '/ideathon/image (21)',
+        '/ideathon/image (22)',
+        '/ideathon/image (23)',
+      ],
+      description: ['The Ideathon, held on 21st September 2024, was a vibrant platform for innovative thinking and creative problem-solving. Teams of students were invited to present solutions to real-world problems across multiple domains. Mentors and industry experts guided participants throughout the brainstorming and presentation stages. The event encouraged a spirit of entrepreneurship and teamwork. It provided a space for young minds to convert ideas into feasible project plans with social impact. Winning ideas were recognized with awards and encouraged for further development and incubation'
+      ],
+    },
+    {
+      title: 'Ganesh Chaturthi',
+      images: [
+        '/ganeshchaturthi/ganesha1',
+        '/ganeshchaturthi/ganesha2',
+        '/ganeshchaturthi/ganesha3',
+        '/ganeshchaturthi/ganesha4',
+        '/ganeshchaturthi/ganesha5',
+        '/ganeshchaturthi/ganesha6',
+        '/ganeshchaturthi/ganesha7',
+        '/ganeshchaturthi/ganesha8',
+        '/ganeshchaturthi/ganesha9',
+      ],
+    },
+    {
+      title: 'IT Club',
+      images: [
+        '/itclub/itclub1',
+        '/itclub/itclub2',
+        '/itclub/itclub3',
+        '/itclub/itclub4',
+        '/itclub/itclub5',
+        '/itclub/itclub6',
+        '/itclub/itclub7',
+        '/itclub/itclub8',
+        '/itclub/itclub9',
+      ],
+    },
+    {
+      title: 'Seminars',
+      images: [
+        '/seminars/seminar1',
+        '/seminars/seminar2',
+        '/seminars/seminar3',
+        '/seminars/seminar4',
+      ],
+    },
+    {
+      title: 'Blockchain Event',
+      images: [
+        '/blockchainevent/blockchain1',
+        '/blockchainevent/blockchain2',
+        '/blockchainevent/blockchain3',
+        '/blockchainevent/blockchain4',
+        '/blockchainevent/blockchain5',
+      ],
+    },
+    {
+      title: 'Ekadashi',
+      images: [
+        '/Ekadashi/Ekadashi.1.jpg',
+        '/Ekadashi/Ekadashi.2.jpg',
+        '/Ekadashi/Ekadashi.3.jpg',
+        '/Ekadashi/Ekadashi.4.jpg',
+        '/Ekadashi/Ekadashi.5.jpg',
+        '/Ekadashi/Ekadashi.6.jpg',
+        '/Ekadashi/Ekadashi.7.jpg',
+      ],
+      description: [
+        'Ashadhi Ekadashi was celebrated on 6th July 2025 with great devotion, joy, traditional fervor. The highlight of the celebration was a beautifully organized Palkhiprocession from Birla College to Birla Mandir, emulating the Warkari pilgrimage.Students dressed in traditional attire, sang devotional songs, and carried symbolic idolswith reverence. The event also featured cultural performances, bhajans, and speecheson the significance of the day. It brought the campus alive with spiritual energy andstrengthened cultural roots among participants. The celebration created a sereneatmosphere that connected everyone to Maharashtra’s devotional heritage'
+      ],
+    },
+    {
+      title: 'Alienwavearenagamingtournament',
+      images: [
+        '/Alienwavearenagamingtournament/Alienwave.1.jpg',
+      
+      ],
+      description: ['The Alienwave Arena Gaming Tournament attracted gaming enthusiasts from across the college. Participants competed in popular games such as Valorant, FIFA, and BGMI, demonstrating strategy and teamwork. The tournament emphasized both entertainment and the growing importance of eSports. Shoutcasting, live streaming, and gaming booths created a high-energy environment. It showcased digital skill, fast reflexes, and the spirit of fair competition. The winners received gaming accessories and recognition among the campus gaming community.'],
+    },
+    {
+      title: 'Codethon',
+      images: [
+        '/Codethon/Codethon.1.jpg',
+        '/Codethon/Codethon.2.jpg',
+        '/Codethon/Codethon.3.jpg',
+        '/Codethon/Codethon.4.jpg',
+        '/Codethon/Codethon.5.jpg',
+      ],
+      description: ['Codethon 2024 was a high-stakes, adrenaline-fueled coding marathon that brought out the best in student programmers. Participants were given complex problem statements and limited time to deliver efficient solutions. The event tested technical skills, logical thinking, and teamwork under time pressure. Multiple rounds filtered the top-performing coders who displayed creativity and precision. It was one of the most competitive and exciting technical events of the academic year. The hackathon fostered innovation under pressure and inspired future developers.'],
+    },
+    {
+      title: 'Creative writing competition',
+      images: [
+        '/Creativewritingcompetition/Creativecomp.1.jpg',
+        '/Creativewritingcompetition/Creativecomp.2.jpg',
+        '/Creativewritingcompetition/Creativecomp.3.jpg',
+      
+      ],
+      description:['Creative Writing Competition focused on enhancing literary skills among participants. Scheduled for March 3, 2025, this event encourages creative expression through various writing formats, including storytelling, poetry, and report writing. Participants can submit entries reflecting themes such as journalism, love, and societal issues. This competition not only aims to foster creativity but also aims to engage students in meaningful discourse on contemporary topics, highlighting an opportunity for students to showcase their talents.'],
+    },
+    {
+      title: 'E-wastecollectionactivity',
+      images: [
+        '/E-wastecollectionactivity/Ewaste.1.jpg',
+        '/E-wastecollectionactivity/Ewaste.2.jpg',
+      ],
+      description:['An institutional e-waste drive was conducted to promote proper electronic waste disposal. Bins were set up across campus for the collection of outdated or unused electronic items. Awareness sessions educated students about the harmful effects of improper e-waste management. The campaign encouraged recycling and responsible consumer behavior. It was an impactful initiative toward building a sustainable and cleaner campus environment. The collected e-waste was sent to certified recyclers, reinforcing the value of responsible disposal.'],
+    },
+    {
+      title: 'Gettogether',
+      images: [
+        '/Gettogether/Gettogether.1.jpg',
+       
+      ],
+    },
+    {
+      title: 'EmpowermingDataAnalytics_Skills_VOISSessiononPowerBI.jpg',
+      images: [
+        '/EmpowermingDataAnalytics_Skills_VOISSessiononPowerBI/DataAnalytics.1.jpg',
+        '/EmpowermingDataAnalytics_Skills_VOISSessiononPowerBI/DataAnalytics.2.jpg',
+        '/EmpowermingDataAnalytics_Skills_VOISSessiononPowerBI/DataAnalytics.3.jpg',
+        '/EmpowermingDataAnalytics_Skills_VOISSessiononPowerBI/DataAnalytics.4.jpg',
+      ],
+    },
+    {
+      title: 'ICCSAS',
+      images: [
+        '/ICCSAS/ICCSAS.1.jpg',
+        '/ICCSAS/ICCSAS.2.jpg',
+        '/ICCSAS/ICCSAS.3.jpg',
+        '/ICCSAS/ICCSAS.4.jpg',
+      ],
+    },
+    {
+      title: 'Mernstackworkshop',
+      images: [
+        '/Mernstackworkshop/Mern.1.jpg',
+        '/Mernstackworkshop/Mern.2.jpg',
+        '/Mernstackworkshop/Mern.3.jpg',
+  
+      ],
+      description: ['The MERN Stack Workshop focused on full-stack web development using MongoDB, Express.js, React.js, and Node.js. Students engaged in real-time coding sessions and built dynamic web applications from scratch. Mentors guided them through component-based design, backend logic, and database integration. It provided hands-on experience that aligned with current industry standards. The workshop significantly enhanced students’ understanding of modern web technologies. Final projects were evaluated by experts and received appreciation for technical depth and user interface design'],
+    },
+    {
+      title: 'Pre-independenceday',
+      images: [
+        '/Pre-independenceday/Pre-independence.jpg',
+       
+      ],
+      description:['On 14th August 2024, Terhkshetra, the official tech club, celebrated Pre-Independence Day with a fusion of technology and patriotism. The event featured themed coding contests, innovation showcases, and presentations on tech in Indian development. Students demonstrated apps and models reflecting national values and futuristic thinking. It created a unique space where technological creativity met national pride. The celebration concluded with patriotic songs and a display of student-made digital projects. It served as a reminder of how youth innovation can contribute to nation-building'],
+    },
+    {
+      title: 'Pravah',
+      images: [
+        '/Pravah/Pravah.1.jpg',
+        '/Pravah/Pravah.2.jpg',
+        '/Pravah/Pravah.3.jpg',
+        '/Pravah/Pravah.4.jpg',
+        '/Pravah/Pravah.5.jpg',
+        '/Pravah/Pravah.6.jpg',
+        '/Pravah/Pravah.7.jpg',
+     
+      ],
+    },
+    {
+      title: 'ReactJS',
+ images: [
+        '/ReactJS/ReactJS.1.jpg',
+        '/ReactJS/ReactJS.2.jpg',
+        '/ReactJS/ReactJS.3.jpg',
+        '/ReactJS/ReactJS.4.jpg',
+        '/ReactJS/ReactJS.5.jpg',
+        '/ReactJS/ReactJS.6.jpg',
+        '/ReactJS/ReactJS.7.jpg',
+        '/ReactJS/ReactJS.8.jpg',
+        '/ReactJS/ReactJS.9.jpg',
+        '/ReactJS/ReactJS.10.jpg',
+        '/ReactJS/ReactJS.11.jpg',
+        
+      ],
+    },
+    {
+      title: 'Republicday',
+      images: [
+        '/Republicday/Republic.1.jpg',
+        '/Republicday/Republic.2.jpg',
+        '/Republicday/Republic.3.jpg',
+        
+      ],
+    },
+    {
+      title: 'Seminar',
+      images: [
+        '/seminar/seminar.1.jpg',
+        '/seminar/seminar.2.jpg',
+        '/seminar/seminar.3.jpg',
+        '/seminar/seminar.4.jpg',
+      ],
+    },
+    {
+      title: 'Star awards',
+      images: [
+        '/Starawards/Star.1.jpg',
+        '/Starawards/Star.2.jpg',
+        '/Starawards/Star.3.jpg',
+        '/Starawards/Star.4.jpg',
+        '/Starawards/Star.5.jpg',
+        '/Starawards/Star.6.jpg',
+      ],
+    },
+    {
+      title: 'Teachers day',
+      images: [
+        '/Teachersday/Teachersday.1.jpg',
+        '/Teachersday/Teachersday.2.jpg',
+        '/Teachersday/Teachersday.3.jpg',
+        '/Teachersday/Teachersday.4.jpg',
+        '/Teachersday/Teachersday.5.jpg',
+        '/Teachersday/Teachersday.6.jpg',
+      ],
+      description:['5th September 2024 was observed with heartfelt celebrations to honor the mentors and faculty. Students organized a cultural program that included performances, games, and personal tributes to their teachers. Fun activities were held to build student-teacher bonding in an informal setting. The program acknowledged the hard work and dedication of the teaching staff. It was a touching and respectful event filled with gratitude and joy. Teachers expressed appreciation for the efforts and creativity shown by students.'],                                                                                                                                                                                                                                                                                                                                                                                            
+    },
+    {
+      title: 'Techexpression2025',
+      images: [
+        '/Techexpression2025/Techexpression.1.jpg',
+        '/Techexpression2025/Techexpression.2.jpg',
+        '/Techexpression2025/Techexpression.3.jpg',
+        '/Techexpression2025/Techexpression.4.jpg',
+        '/Techexpression2025/Techexpression.5.jpg',
+        '/Techexpression2025/Techexpression.6.jpg',
+        '/Techexpression2025/Techexpression.7.jpg',
+        '/Techexpression2025/Techexpression.8.jpg',
+        '/Techexpression2025/Techexpression.9.jpg',
+        '/Techexpression2025/Techexpression.10.jpg',
+      ],
+      description: ['TechXpression is an intercollegiate tech festival scheduled for January 31st and February 1st, 2025. The theme, "AI Nexus: Bridging the Innovation with Security,"highlights the convergence of technology and cybersecurity. Participants can engage in various competitions, including a Hackathon with a prize pool of ₹50,000, along with other events like a Tech Treasure Hunt, TechStar Unplugged, and AI Cyber Forensics, each offering additional prizes. This event promises an exciting experience for tech enthusiasts and encourages innovation and collaboration among students.'],
+    },
+    {
+      title: 'Yogaday',
+      images: [
+        '/Yogaday/Yoga.1.jpg',
+        '/Yogaday/Yoga.2.jpg',
+        '/Yogaday/Yoga.3.jpg',
+      ],
+      description: ['On 20th June 2024, the institute enthusiastically celebrated the International Day of Yoga to promote physical and mental well-being. A large number of students and faculty members participated in guided yoga sessions. The program emphasized breathing techniques, stretching exercises, and mindfulness practices. The event was aimed at reducing academic stress and encouraging a healthy lifestyle. It successfully fostered a sense of peace, awareness, and discipline among all participants. The celebration ended with a collective meditation session promoting inner balance and harmony.'
+      ],
+    },
+    {
+      title: 'Tectiranga',
+      images:[
+        'Techtiranga/Techtiranga.1.jpg',
+        'Techtiranga/Techtiranga.2.jpg',
+      ],
+      description: ['The Techtrianga Competition featured a variety of technical contests such as debugging challenges, coding battles, and technical quizzes. Students competed across multiple levels, showcasing logical thinking, speed, and technical expertise. The event fostered peer learning and team collaboration under pressure. It was designed to cultivate innovation, practical knowledge, and healthy competition. Participants demonstrated outstanding technical aptitude and enthusiasm throughout the event. The winners were awarded certificates and prizes, motivating others to sharpen their skills.'
+      ],
+    },
+    {
+      title:'Technovinayak',
+      images:[
+        'TechnoVinayak/TechnoVinayak.1.jpg',
+        'TechnoVinayak/TechnoVinayak.2.jpg',
+        'TechnoVinayak/TechnoVinayak.3.jpg',
+        'TechnoVinayak/TechnoVinayak.4.jpg',
+        'TechnoVinayak/TechnoVinayak.5.jpg',
+        'TechnoVinayak/TechnoVinayak.6.jpg',
+        'TechnoVinayak/TechnoVinayak.7.jpg',
+        'TechnoVinayak/TechnoVinayak.8.jpg',
+        'TechnoVinayak/TechnoVinayak.9.jpg',
+        'TechnoVinayak/TechnoVinayak.10.jpg',
+        'TechnoVinayak/TechnoVinayak.11.jpg',
+        'TechnoVinayak/TechnoVinayak.12.jpg',
+      ],
+      description: ['On 4th September 2024, the campus celebrated Techno Vinayak by merging Ganesh Utsav with innovation. Students built tech-integrated, eco-friendly Ganesh idols using sustainable materials and simple electronics. Workshops were also conducted on environmentally safe celebrations using smart solutions. The celebration preserved tradition while promoting green practices and technology use. It was a creative example of how festivals and science can co-exist meaningfully. The event concluded with a thoughtful message on responsible celebrations and sustainability'],
+    
+    },
+    {
+      title:['WorldWideWebinformationdrive'],
+      images:[
+        '/WorldWideWebinformationdrive/WWW.1.jpg',
+        '/WorldWideWebinformationdrive/WWW.2.jpg',
+        '/WorldWideWebinformationdrive/WWW.3.jpg',
+      ],
+      description: ['On 5th August 2024, a special information drive was conducted to raise awareness about the history, evolution, and safe use of the World Wide Web. Seminars were held on topics like data privacy, cyberbullying, and digital literacy. Students participated in online safety quizzes and poster competitions. Experts from the tech industry shared insights on internet governance and responsible digital behavior. The event equipped students with the knowledge to become ethical and informed internet users. The campaign emphasized the importance of digital responsibility in today’s connectedworld.'
+
+      ],
+    },
+]
+
+export default highlights;
